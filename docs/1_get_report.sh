d8e8ea8703fb5731490fcbef41261502593151f1
lconde-ucl <l.conde@ucl.ac.uk>
2024-07-17 14:49:40 +0100

    add website

---
+++ docs/1_get_report.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+
+
+module load r/recommended
+module load pandoc
+
+
+# this does nto preserve the lumen theme
+#pandoc AWS_doc.md -o AWS_doc.html
+
+R --vanilla  -e "rmarkdown::render('AWS_doc.Rmd',output_file='index.html')"
+
